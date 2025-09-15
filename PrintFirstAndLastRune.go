84e8ed00776f9efba52fe4fd2ca35b8798a513ee
Sasha <sshpolar@gmail.com>
2025-09-15 11:20:38 +0200

    Yexamen2

---
+++ PrintFirstAndLastRune.go
@@ -0,0 +1,19 @@
+package exam2
+
+import "github.com/01-edu/z01"
+
+func PrintFirstAndLastRune(s string) {
+
+	for s := 0; s <= range(s); {
+
+		z01.PrintRune(s string)
+		return 0
+	}
+
+	for s:= 0; s >= range(s); {
+
+		z01.PrintRune(s string)
+		return 0
+	}
+}
+
